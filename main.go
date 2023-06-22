0d47a5a75f0ae458237900259c38446d979b91b6
Akhil Singh <akhil.singh@infra.market>
2023-06-23 00:10:48 +0530

    Add method to delete word

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrRecordNotFound = errors.New("record not found")
+
+func FormatErrRecordNotFound(word string) error {
+	return fmt.Errorf("%s: %w", word, ErrRecordNotFound)
+}
+
 type TrieNode struct {
 	children map[rune]*TrieNode
-	//To store frequency of string incase of duplicates
+	//To store frequency of string in case of duplicates
 	count int
 }
 
@@ -35,4 +46,46 @@ func (t *Trie) Search(word string) bool {
 	}
 	return node.count != 0
 }
-func main() {}
+
+func (t *Trie) Delete(word string) {
+	t.delete(t.root, word, 0)
+}
+
+func (t *Trie) delete(node *TrieNode, word string, index int) *TrieNode {
+	if index == len(word) {
+		if node.count != 0 {
+			node.count--
+		}
+		if len(node.children) == 0 {
+			node = nil
+		}
+		return node
+	}
+
+	ch := rune(word[index])
+	if child, ok := node.children[ch]; ok {
+		node.children[ch] = t.delete(child, word, index+1)
+	}
+
+	if len(node.children) == 0 && node.count == 0 {
+		node = nil
+	}
+
+	return node
+}
+
+func main() {
+	t := NewTrie()
+	t.Insert("go")
+	t.Insert("golang")
+	t.Insert("gopher")
+	t.Insert("godoc")
+	t.Insert("good")
+
+	fmt.Println(t.Search("go"))
+	fmt.Println(t.Search("gopher"))
+	fmt.Println(t.Search("godzilla"))
+
+	t.Delete("gopher")
+	fmt.Println(t.Search("gopher"))
+}
