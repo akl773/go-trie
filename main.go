1bccc64d7985be5ec7f383cdba93a99d58ecf10a
Akhil Singh <akhil.singh@infra.market>
2023-06-22 23:42:38 +0530

    Add method to insert word

--- main.go
+++ main.go
@@ -13,4 +13,15 @@ func NewTrie() *Trie {
 	return &Trie{root: &TrieNode{children: make(map[rune]*TrieNode)}}
 }
 
+func (t *Trie) Insert(word string) {
+	node := t.root
+	for _, ch := range word {
+		if _, ok := node.children[ch]; !ok {
+			node.children[ch] = &TrieNode{children: make(map[rune]*TrieNode)}
+		}
+		node = node.children[ch]
+	}
+	node.isWord = true
+}
+
 func main() {}
